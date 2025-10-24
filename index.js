c97707e147532c4a15246ccb8aa6144f0d6406dc
Opilane TTHK <opilane@tthk.ee>
2025-10-24 14:12:30 +0300

    As an app developer I want to be able to delete a game [#430020]

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const app = require('express')()
-const port = 8080
+const port = 3488
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml')
+
+const games = [
+    {id:1, name: "Wicher 3", price:29.99},
+    {id:2, name: "Cyberpunk 2077", price:59.99}
+]
 
 app.get('/games', (req, res) => {
-    res.send(["Wicher 3", "Cyberpunc 2077"])
+    res.send(games)
+})
+
+app.get('/games/:id', (req, res) => {
+
+    if (typeof games[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+    res.send(games[req.params.id - 1])
+})
+
+app.post('/games', (req,res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({ error: 'one or all params are missing'})
+    }
+    let game = {
+        id: games.lenght + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+
+    games.push(game)
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game)
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
-    console.log('API up at: http://localhost:' + port)
+    console.log(`API up at: http://localhost:${port}`)
+})
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + `://${req.headers.host}`
+}
+
+app.delete('/games/:id', (req,res) => {
+    if (typeof games[req.params.id -1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    games.splice(req.params.id - 1, 1)
+
+    res.status(204).send({error: "No content"})
 })
